8dab2c17faf318b2d7bf81d9c6fafe60e7d12e5e
Egor Bukraba <eb@bankexfoundation.org>
2018-02-25 17:51:14 +0000

    Update constants.js to actual version

--- constants.js
+++ constants.js
@@ -54,7 +54,6 @@ exports.MY_COPY_FOLDERS = [
     // use this for folders you want to be copied in to Client dist
     // src/assets and index.html are already copied by default.
     // format is { from: 'folder_name', to: 'folder_name' }
-    { from: 'build/contracts', to: 'assets/contracts' }
 ]
 
 exports.MY_POLYFILL_DLLS = [
